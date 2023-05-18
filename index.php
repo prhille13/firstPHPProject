998ac275e8c702eb78c183b617f907966839b86e
Peter Hille <peter.hille13@gmal.com>
2023-05-18 17:17:47 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>My First PHP Site</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello, World!";
+        ?>
+    </h1>
+</body>
+</html>
